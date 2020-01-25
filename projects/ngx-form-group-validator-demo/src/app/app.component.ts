3e366ae4dea6bd7651313d4903a9fdf3d174a348
Blazej456 <blazej456@gmail.com>
2020-01-25 13:26:42 +0100

    add demo application to workspace

---
+++ projects/ngx-form-group-validator-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fgv-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'ngx-form-group-validator-demo';
+}
