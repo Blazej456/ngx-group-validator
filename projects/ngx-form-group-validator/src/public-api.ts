78e201e1baf7630ebb575b6a70172448a8237d99
Blazej456 <blazej456@gmail.com>
2020-02-01 12:07:14 +0100

    remove helper and expand example one

--- projects/ngx-form-group-validator/src/public-api.ts
+++ projects/ngx-form-group-validator/src/public-api.ts
@@ -1,6 +1,5 @@
 /*
  * Public API Surface of ngx-form-group-validator
  */
-export * from './lib/ngx-validators-helpers';
 export * from './lib/ngx-group-validators.model';
 export * from './lib/ngx-group-validators';
