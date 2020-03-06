8e6b3b24f70a4937792b59aaf2f10a34011064cb
Blazej456 <blazej456@gmail.com>
2020-03-06 21:34:00 +0100

    updating dep version on bumping lib version

--- bump_version.sh
+++ bump_version.sh
@@ -9,6 +9,8 @@ VERSION=$(cat package.json \
 
 echo $VERSION
 
+sed -i 's/"ngx-group-validator": "[0-9]\.[0-9][0-9]*\.[0-9][0-9]*"/"ngx-group-validator": "'$VERSION'"/g' package.json
+
 cd projects/ngx-group-validator || return
 
 npm version $VERSION --git-tag-version false
